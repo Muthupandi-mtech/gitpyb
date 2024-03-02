8dba55be2889e97da614a9e759af7333b79d8e33
root <root@DESKTOP-LKMP6H8>
2024-03-02 19:09:31 +0530

    code changed

--- src/main/python/example_hello.py
+++ src/main/python/example_hello.py
@@ -10,18 +10,6 @@ def hello_world(out):
     """
     out.write("Hello World!\n")
 
-"""
-
-Below:
-
- > Required in order to run the documentation checker on this code.
-
- > The comment
-     pragma: no cover
-   means that this block of code will not be checked for unit test
-   coverage by the `coverage` module.
-
-"""
 
 if __name__ == "__main__": # pragma: no cover
     import doctest
